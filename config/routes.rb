e7815d4e6b2d48e4bb4228af3302eca0cecc1de5
Lucas Charles <lucascharlesz@hotmail.com>
2017-11-07 16:40:31 -0200

    Aula 022.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   namespace :backoffice do
-    get 'categories/index'
+    resources :categories, except: [:show]
+    get 'dashboard', to: 'dashboard#index'
   end
 
   namespace :site do
     get 'home/index'
-  end
-
-  namespace :backoffice do
-    get 'dashboard', to: 'dashboard#index'
   end
 
   devise_for :users
