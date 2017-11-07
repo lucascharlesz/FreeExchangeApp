15664df391c00fd549c6c466db956cff1b9f46e3
Lucas Charles <lucascharlesz@hotmail.com>
2017-11-07 09:59:44 -0200

    Aula 010.

---
+++ app/models/advertisement.rb
@@ -0,0 +1,4 @@
+class Advertisement < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107112630) do
+ActiveRecord::Schema.define(version: 20171107115312) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "user_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_advertisements_on_category_id"
+    t.index ["user_id"], name: "index_advertisements_on_user_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "description"
