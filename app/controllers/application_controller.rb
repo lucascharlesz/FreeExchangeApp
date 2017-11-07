aa8085f51edad43fb21316c52ef4befbccbad67e
Lucas Charles <lucascharlesz@hotmail.com>
2017-11-07 14:39:29 -0200

    Aula 020.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  layout :set_devise_layout
+
+  protected
+  def set_devise_layout
+    if devise_controller? && resource_name == :user
+      "backoffice_login"
+    else
+      "application"
+    end
+  end
 end
